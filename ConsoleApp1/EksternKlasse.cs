9d9a5a90556d4f1b7ef4b06dafb403c232b8ebac
RaniaEG <79601775+RaniaEG@users.noreply.github.com>
2026-02-04 13:18:52 +0100

    Arv og tilgang

---
+++ ConsoleApp1/EksternKlasse.cs
@@ -0,0 +1,10 @@
+namespace ConsoleApp1
+{
+    public class EksternKlasse : TestTilgangKlasse
+    {
+        public void TestTilgangEkstern()
+        {
+            Console.WriteLine($"klassen kan få tilgang til {protectedInternalFelt}");
+        }
+    }
+}
