acba0cf8c07c4fdfcf322b5c6bca705c2839b221
RaniaEG <79601775+RaniaEG@users.noreply.github.com>
2026-02-04 13:51:32 +0100

    Arv og tilgang

--- Forelesning2/TestTilgangKlasse.cs
+++ Forelesning2/TestTilgangKlasse.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace Forelesning2
 {
+    /////// Vi skal teste tilgangen til de feltene i denne klassen fra subklasse (ChildTestTilgangKlasse.cs) inn i Forelesning2 prosjekt, vanlig kasse uten arv (VanligKlasse.cs) inn i Forelesning2 prosjekt og en subklasse (EksternKlasse.cs) inn i ConsoleApp1 prosjekt (annet prosjekt)
     public class TestTilgangKlasse
     {
-        private string privatFelt = "Dette er et privat felt"; // tilgjengelig kun i denne klassen (kan bruke set og get metoder her)
-        protected string protctedFelt = "Dette er et protected felt";
-        private protected string privatProtectedFelt = "Dette er et privat protected felt";
-        protected internal string protectedInternalFelt = "Dette er et protected internal felt";
+        private string privatFelt = "Dette er et privat felt"; // tilgjengelig kun for denne klassen (kan bruke set og get metoder her)
+        protected string protctedFelt = "Dette er et protected felt";  // tilgjengelig kun for denne klassen, subklassen (ChildTestTilgangKlasse.cs) og subklassen (EksternKlasse.cs)
+        private protected string privatProtectedFelt = "Dette er et privat protected felt"; // tilgjengelig kun for denne klassen og subklassen (ChildTestTilgangKlasse.cs)
+        protected internal string protectedInternalFelt = "Dette er et protected internal felt"; // tilgjengelig for denne klassen, subklassen (ChildTestTilgangKlasse.cs) i samme prosjekt, vanlig klasse uten arv (VanligKlasse.cs) i samme prosjekt og subklassen (EksternKlasse.cs) i annet prosjekt
     }
 }
