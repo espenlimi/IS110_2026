d5c26a4dc1166497eee9132c09e8f0d7507afc70
RaniaEG <79601775+RaniaEG@users.noreply.github.com>
2026-02-25 17:06:00 +0100

    Polymorfisme og Interfacer

---
+++ Polymorfisme/Polymorfisme/IKjørbar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorfisme
+{
+    internal interface IKjørbar
+    {
+        void StarteMotor();
+        void StoppeMotor();
+        void Kjør();
+
+
+    }
+}
