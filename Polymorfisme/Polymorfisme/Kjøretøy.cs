04fa231a1f1949b0b524897b57acc1d6c7d22d43
RaniaEG <79601775+RaniaEG@users.noreply.github.com>
2026-02-26 08:17:36 +0100

    Polymorfisme og interfacer

---
+++ Polymorfisme/Polymorfisme/Kjøretøy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorfisme
+{
+    internal class Kjøretøy : IKjørbar, IServicebar
+    {
+        public int KiloMeterKjørtSidenSisteService {  get; set; }
+        
+        public void Kjør()
+        {
+            Console.WriteLine("Kjøretøyet kjører....");
+        }
+
+        public void StarteMotor()
+        {
+            Console.WriteLine("Kjøretøyet starter motoren....");
+        }
+
+        public void StoppeMotor()
+        {
+            Console.WriteLine("Kjøretøyet stopper motoren....");
+        }
+
+        public bool TrengerService()
+        {
+            return (KiloMeterKjørtSidenSisteService >= 15000);
+            
+        }
+
+        public void UtførService()
+        {
+            Console.WriteLine("Kjøretøyet er under service....");
+            KiloMeterKjørtSidenSisteService = 0;
+        }
+    }
+}
---
+++ Polymorfisme/Polymorfisme/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Polymorfisme;
+
+Console.WriteLine("Hello, World!");
+
+Kjøretøy k = new PersonBil();
+Kjøretøy t = new PersonBil();
+Kjøretøy p = new PersonBil();
+List<IServicebar> services = new List<IServicebar>();
+services.Add(k);
+services.Add(t);
+services.Add(p);
+services.Add(new PersonBil{KiloMeterKjørtSidenSisteService = 20000});
+
+foreach(IServicebar service in services)
+{
+    if (service.TrengerService())
+        service.UtførService();
+}
+
+// for å teste om det skal printe ut en gang til etter at antall kilometere igjen etter de ble satt til 0
+foreach (IServicebar service in services)
+{
+    if (service.TrengerService())
+        service.UtførService();
+}
+
